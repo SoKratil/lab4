751bd9b6e86180861caeaf9944a64a8f204d8acf
Dmitry Avdeevma <avdeev-d2002@mail.ru>
2023-03-14 20:23:03 +0700

    lil up

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -21,21 +21,31 @@ class MainActivity : AppCompatActivity()
     fun clik(view: View)
     {
         val edittext : EditText = findViewById(R.id.addnum)
-        val result : TextView = findViewById(R.id.result)
-        result.setText(daywek(edittext.text.toString()))
+        val result : TextView = findViewById(R.id.add)
+        val result2 : TextView = findViewById(R.id.add2)
+        val text = edittext.text.toString().toInt()*edittext.text.toString().toInt()
+        result.setText("1/$text")
+        result2.setText(math(edittext.text.toString()))
 
 
     }
 
-    private fun daywek(num : String): String {
-        if (num == "1") return "Понедельник"
-        else  if (num == "2") return "Вторник"
-        else if (num == "3") return "Среда"
-        else if (num == "4") return "Четверг"
-        else if (num == "5") return "Пятница"
-        else if (num == "6") return "Суббота"
-        else if (num == "7") return "Воскресенье"
-        else  return "Вы ввели не 1-7"
+    private fun math(num : String): String
+    {
+        var sum = 0.0
+        if (num == null || !num.all { it.isDigit() })
+        {
+            return "Ошибка: x должен быть числом."
+        }
+        else
+        {
+            for (i in 1..num.toInt())
+            {
+                sum += 1.0 / (i*i)
+            }
+            val formattedValue = String.format("%.3f", sum)
+            return "Сумма чисел от 1 до 1/$num равна $formattedValue."
+        }
 
     }
 
