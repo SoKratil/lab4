b5e6a1d3ef6a502da3b49c13696ce9604d76592a
Dmitry Avdeevma <avdeev-d2002@mail.ru>
2023-03-14 22:57:51 +0700

    upd

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -21,32 +21,20 @@ class MainActivity : AppCompatActivity()
     fun clik(view: View)
     {
         val edittext : EditText = findViewById(R.id.addnum)
-        val result : TextView = findViewById(R.id.add)
         val result2 : TextView = findViewById(R.id.add2)
-        val text = edittext.text.toString().toInt()*edittext.text.toString().toInt()
-        result.setText("1/$text")
-        result2.setText(math(edittext.text.toString()))
-
+        result2.setText(countLatinLetters(edittext.text.toString()))
 
     }
 
-    private fun math(num : String): String
+    fun countLatinLetters(inputString: String): String
     {
-        var sum = 0.0
-        if (num == null || !num.all { it.isDigit() })
-        {
-            return "Ошибка: x должен быть числом."
-        }
-        else
-        {
-            for (i in 1..num.toInt())
-            {
-                sum += 1.0 / (i*i)
+        var count = 0
+        for (char in inputString) {
+            if (char in 'a'..'z' || char in 'A'..'Z') {
+                count++
             }
-            val formattedValue = String.format("%.3f", sum)
-            return "Сумма чисел от 1 до 1/$num равна $formattedValue."
         }
-
+        return count.toString()
     }
 
 }
